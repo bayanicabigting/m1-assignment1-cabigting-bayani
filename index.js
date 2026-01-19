cceaaa425a6e53a2c64f4f7d5bc69eb32453b6dc
Bayani Cabigting <noodlenavi@gmail.com>
2026-01-18 19:14:11 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+function greet() {
+    let name = prompt("What is your name?");
+    alert("Hello, " + name);
+    let age = parseInt(prompt("How old are you?"));
+    let birthday = confirm("Please click OK if you have had your birthday this year. Click Cancel if you have not.")
+    if (birthday === true) { 
+            alert("You were born in the year " + (new Date().getFullYear() - age))
+    }
+    else {
+        alert("You were born in the year "+ (new Date().getFullYear() - age - 1))
+    }
+}
